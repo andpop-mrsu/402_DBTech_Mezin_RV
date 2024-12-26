4fcf65c90b40fcd6bd234840547024c6bbf74b22
Your Name <roma345m@mail.ru>
2024-12-26 16:06:36 +0300

    Task03

---
+++ Task02/hangman/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'staticnur/hangman',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '993e475646e9e38b155954d31c4c95fed4901187',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'staticnur/hangman' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '993e475646e9e38b155954d31c4c95fed4901187',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wp-cli/php-cli-tools' => array(
+            'pretty_version' => 'v0.12.0',
+            'version' => '0.12.0.0',
+            'reference' => '2d1da4751f47622b56e6616c87b2fb53decee4e5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wp-cli/php-cli-tools',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
